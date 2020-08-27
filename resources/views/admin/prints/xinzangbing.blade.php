907392a8286569e624b1acfe092b2ea380aaa26a
1078979110 <1078979110@qq.com>
2020-08-27 18:04:47 +0800

    修改创建直播间

--- resources/views/admin/prints/xinzangbing.blade.php
+++ resources/views/admin/prints/xinzangbing.blade.php
@@ -1,42 +1,42 @@
 <div class="panel panel-default">
   <div class="panel-heading">{{$tabletitle[0]}}</div>
   <div class="panel-body" id="table" >
-    <table  style="font-size:14px" cellpadding="0" cellspacing="0">
+    <table  style="font-size:12px" cellpadding="0" cellspacing="0">
     	<tbody>
     		<tr style="line-height: 30px; height: 30px"><td colspan="14" align="center" style="font-size: 16px; font-weight:bold">{{$tabletitle[0]}}</td></tr>
     		<tr style="line-height: 30px; height: 30px"><td colspan="7">{{$tabletitle[1]}}</td><td colspan="7">{{$tabletitle[2]}}</td></tr>
     		<tr style="line-height: 30px; height: 30px"><td colspan="7">{{$tabletitle[3]}}</td><td colspan="7">{{$tabletitle[4]}}</td></tr>
     		<tr style="line-height: 30px; height: 30px">
     			<td style="border-left:1px solid #000;border-right:1px solid #000;border-top:1px solid #000; width:30px" align="center">{{$datatitle[0]}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:50px">{{$datatitle[1]}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:150px">{{$datatitle[2]}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:50px">{{$datatitle[3]}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:50px">{{$datatitle[4]}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:250px">{{$datatitle[5]}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:50px">{{$datatitle[6]}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:50px">{{$datatitle[7]}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:50px">{{$datatitle[8]}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:50px">{{$datatitle[9]}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:80px">{{$datatitle[10]}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:100px">{{$datatitle[11]}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:100px">{{$datatitle[12]}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000; ">{{$datatitle[13]}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:50px" align="center">{{$datatitle[1]}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:150px" align="center">{{$datatitle[2]}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:50px" align="center">{{$datatitle[3]}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:50px" align="center">{{$datatitle[4]}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:250px" align="center">{{$datatitle[5]}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:50px" align="center">{{$datatitle[6]}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:50px" align="center">{{$datatitle[7]}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:50px" align="center">{{$datatitle[8]}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:50px" align="center">{{$datatitle[9]}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:80px" align="center">{{$datatitle[10]}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:100px" align="center">{{$datatitle[11]}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000; width:100px" align="center">{{$datatitle[12]}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000; " align="center">{{$datatitle[13]}}</td>
     		</tr>
     		@foreach($lists as $key => $list)
     		<tr style="line-height: 30px; height: 30px">
     			<td style="border-left:1px solid #000;border-right:1px solid #000;border-top:1px solid #000" align="center">{{$key+1}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000">{{$list['medicinalnum']}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000">{{$list['medicinal']}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000">{{$list['specification']}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000">{{$list['boxformat']}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000">{{$list['manufactur']}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000" align="center">{{$list['medicinalnum']}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000" align="center">{{$list['medicinal']}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000" align="center">{{$list['specification']}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000" align="center">{{$list['boxformat']}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000" align="center">{{$list['manufactur']}}</td>
     			<td style="border-right:1px solid #000;border-top:1px solid #000" align="center">{{$list['num']}}</td>
     			<td style="border-right:1px solid #000;border-top:1px solid #000" align="center">{{$list['unit']}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000">{{$list['price']}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000">{{$list['batchnumber']}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000">{{$list['invalidate']}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000">{{$list['registnum']}}</td>
-    			<td style="border-right:1px solid #000;border-top:1px solid #000">{{$list['registivalidate']}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000" align="center">{{$list['price']}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000" align="center">{{$list['batchnumber']}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000" align="center">{{$list['invalidate']}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000" align="center">{{$list['registnum']}}</td>
+    			<td style="border-right:1px solid #000;border-top:1px solid #000" align="center">{{$list['registivalidate']}}</td>
     			<td style="border-right:1px solid #000;border-top:1px solid #000"></td>
     		</tr>
     		@endforeach
@@ -60,7 +60,7 @@ var LODOP;
 var isnull= false;
 var data = {!! $jsondata  !!};
 function print_view(){
-	LODOP.ADD_PRINT_TABLE(40,10,"RightMargin:0.9cm",'100%',document.getElementById('table').innerHTML);
+	LODOP.ADD_PRINT_TABLE(40,10,"RightMargin:0.3cm",'100%',document.getElementById('table').innerHTML);
 	check_isnull(data);
 	if(isnull == true){
 		isprint = confirm('该出货单中存在未填写的项,确定打印？');
@@ -70,7 +70,7 @@ function print_view(){
 	}
 }
 function print(){
-	LODOP.ADD_PRINT_TABLE(40,10,"RightMargin:0.9cm",'100%',document.getElementById('table').innerHTML);
+	LODOP.ADD_PRINT_TABLE(40,10,"RightMargin:0.3cm",'100%',document.getElementById('table').innerHTML);
 	check_isnull(data);
 	if(isnull == true){
 		isprint = confirm('该出货单中存在未填写的项,确定打印？');
