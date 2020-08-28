7650f4c8562b0b6c0a1d19f12cd4c0ca8f55adb2
1078979110 <1078979110@qq.com>
2020-08-28 20:16:50 +0800

    修改创建直播间

--- app/Admin/Controllers/OrderController.php
+++ app/Admin/Controllers/OrderController.php
@@ -76,7 +76,7 @@ class OrderController extends AdminController
                 $filter->equal('buyertype','订单类型')->select($arr);
             }
         });
-        $grid->model()->orderBy('created_at');
+        $grid->model()->orderBy('created_at','desc');
         $grid->column('orderid','订单号');
         $grid->column('totalprice', '订单金额');
         $grid->column('orderinfo','订单详情')->display(function(){
