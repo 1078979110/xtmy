234e50fbf9b85a9ca31a1e1ccae95820f9347304
1078979110 <1078979110@qq.com>
2020-08-31 16:50:28 +0800

    修改创建直播间

--- app/Admin/Controllers/SettingController.php
+++ app/Admin/Controllers/SettingController.php
@@ -12,6 +12,7 @@ class SettingController extends AdminController{
         $content->title('账号设置');
         $telephone = Admin::user()->username; 
         $info = Salelist::where('telephone',$telephone)->get()->toArray(true);
+        $info['username'] = $info['telephone'];
         if(empty($info)){
             $infos = DB::table('admin_users')->where('username', $telephone)->get()->toArray(true);
             $info = (array)$infos[0];
