ea1e36f4bbf5bc78a4c9cdff28362e15b7d48e29
1078979110 <1078979110@qq.com>
2020-09-08 09:32:27 +0800

    ss

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -462,7 +462,7 @@ class IndexController extends Controller {
 
                         $medicinalinfo = Medicinal::where('medicinalnum', $v['产品货号'])->first();
                         if(!$medicinalinfo){
-                            throw new \Exception('第'.$k.'行产品在产品库不存在');
+                            throw new \Exception('第'.($k+1).'行产品在产品库不存在');
                         }
                         $_info = [
                             'medicinalid'=> $medicinalinfo->id,
