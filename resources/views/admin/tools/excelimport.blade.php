873b637445770c9378ca60b1fb2ecefa96a39e23
1078979110 <1078979110@qq.com>
2020-09-11 14:27:15 +0800

    ss

--- resources/views/admin/tools/excelimport.blade.php
+++ resources/views/admin/tools/excelimport.blade.php
@@ -1,1 +1,1 @@
-<button class="btn btn-info excel">导入数据</button>
+<button class="btn btn-sm btn-info excel">导入数据</button>
--- resources/views/admin/tools/setprice.blade.php
+++ resources/views/admin/tools/setprice.blade.php
@@ -1,3 +1,3 @@
 <div class="btn-group" style="margin-right: 5px">
-<button type="button" class="btn btn-success" id="setadmin"><i class="fa fa-dollar"></i> 设置价格</button>
+<button type="button" class="btn btn-sm btn-success" id="setadmin"><i class="fa fa-dollar"></i> 设置价格</button>
 </div>
