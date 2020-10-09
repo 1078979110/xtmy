190c1dd0c2947a52dea2afae5b6fb538fdd71a26
1078979110 <1078979110@qq.com>
2020-10-09 17:23:29 +0800

    ss

--- app/Admin/Controllers/OrderController.php
+++ app/Admin/Controllers/OrderController.php
@@ -71,11 +71,6 @@ class OrderController extends AdminController
                 $sarr = [];
                 foreach ($orderinfo as $key => $val) {
                     $medicinal = Order::medicinalInfo($val->medicinal_id);
-                    if(empty($medicinal)){
-                        var_dump($val);
-                        var_dump($medicinal);
-                        exit;
-                    }
                     $sarr[$key]['id'] = $key + 1;
                     $sarr[$key]['medicinal'] = $medicinal->medicinal;
                     $sarr[$key]['medicinalnum'] = $medicinal->medicinalnum;
