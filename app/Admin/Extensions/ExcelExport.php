122fc10566d5759a33b275d1f93fe7d555945860
1078979110 <1078979110@qq.com>
2020-10-23 16:19:37 +0800

    添加未分库导出拦截

--- app/Admin/Extensions/ExcelExport.php
+++ app/Admin/Extensions/ExcelExport.php
@@ -5,11 +5,24 @@ use Encore\Admin\Facades\Admin;
 use Encore\Admin\Grid\Exporters\AbstractExporter;
 use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Support\Facades\DB;
+use \Exception;
 class ExcelExport extends AbstractExporter
 {
     public function export()
     {
         $data = $this->getData();
+        $str = '';
+        foreach ($data as $key=>$val){
+            if($val['splitstatus'] == 0){
+                $str .= $val['orderid'].',';
+            }
+        }
+        if($str != ''){
+            $str = '存在未分库订单,请排除未分库订单或者等待订单分库后再进行导出，未分库订单为：'.$str;
+            admin_warning('导出订单', $str);
+            echo "<script>alert('{$str}');window.location.href = '/admin/orders'</script>";
+            exit;
+        }
         if(Admin::user()->isRole('administrator')){
             $cellData = $this->adminDataTools($data);
             $finename = date('订单导出'.'Y-m-d H-i-s', time());
